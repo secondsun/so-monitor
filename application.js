9c9fc22c9d38a8e6e386b80a0666b5df62bd4e7b
Summers Pittman <secondsun@gmail.com>
2016-12-06 12:15:55 -0500

    Some refactoring

--- application.js
+++ application.js
@@ -4,19 +4,17 @@
 // var cors = require('cors');
 var config = require("./config.json")
 var stackexchange = require('stackexchange');
-var J2M = require('j2m');
+var issueUtils = require("./monitor/issue_utils.js")(config);
 var options = { version: 2.2 };
 var context = new stackexchange(options);
 
 var jira_config = config.jira_connection;
 var stackexchange_query = config.stackexchange_query;
-var issue_template = config.issue_template;
+
 
 JiraApi = require('jira').JiraApi;
 
 var jira = new JiraApi(jira_config.schema, jira_config.host, jira_config.port, jira_config.username, jira_config.password, jira_config.version);
-
-//
 
 var filter = {
   key: stackexchange_query.key,
@@ -31,47 +29,27 @@ var filter = {
 context.questions.questions(filter, function (err, results) {
   if (err) throw err;
 
-  var body = J2M.toJ(results.items[0].body);
-  body = body.replace(/\[[\/]*p\]/g, '');
-  body = body.replace(/\[[\/]*pre\]/g, '');
-  body = body.replace(/\[\/*code\]/g, '{code}');
-  body = body.replace(/\[a[^\]]* href="([^"]*)"[^\]]*\]([^\[]*)\[\/a\]/g, '[$2|$1]');
-  console.log(body);
+  results.items.forEach(function (item) {
 
-  var issue = {
-    "fields": {
-      "project": {
-        "id": issue_template.project_id
+    var issue = issueUtils.createIssue(item);
 
-      },
-      "summary": results.items[0].title,
-      "description": "{quote}" + body + "{quote} \n\n" + results.items[0].link,
-      "issuetype": {
-        "name": "Task"
-      },
-      "priority": {
-        "name": "Minor"
+    jira.addNewIssue(issue, function (err, response) {
+      if (err) {
+        console.log('✘ We have an error - ' + JSON.stringify(err));
+      } else if (response.self !== undefined) {
+
+        console.log("✔ Created Ticket - " + jira_config.schema + "://" + jira_config.host + "/browse/" + response.key + " - " + response.self); // key = Issue ID, self = Link to Jira Issue
+        jira.updateIssue(response.key, { "update": { "labels": [{ "add": 'team-developer-experience' }] } }, function (err, response) {
+          if (err) {
+            console.log('✘ We have an error - ' + JSON.stringify(err));
+          } else {
+            console.log("✔ Added Label team-developer-experience");
+          }
+
+        });
       }
-    }
-  };
 
-  jira.addNewIssue(issue, function (err, response) {
-    if (err) {
-      console.log('✘ We have an error - ' + JSON.stringify(err));
-    } else if (response.self !== undefined) {
-
-      console.log("✔ Created Ticket - " + jira_config.schema + "://" + jira_config.host + "/" + response.key + " - " + response.self); // key = Issue ID, self = Link to Jira Issue
-      jira.updateIssue(response.key, { "update": { "labels": [{ "add": 'team-developer-experience' }] } }, function (err, response) {
-        if (err) {
-          console.log('✘ We have an error - ' + JSON.stringify(err));
-        } else {
-          console.log("✔ Added Label team-developer-experience");
-        }
-        process.exit(0);
-      });
-    }
-
-
-
+    });
   });
+
 });
---
+++ monitor/issue_utils.js
@@ -0,0 +1,50 @@
+var J2M = require('j2m');
+
+var IssueUtils = function (config) {
+
+    this.formatQuestion = formatQuestion;
+    this.createIssue = createIssue;
+    this.issue_template = config.issue_template;
+
+    /**
+     * @param question : String the question body from stack overflow.
+     * @returns the question in a JIRA compatible markdown. 
+     */
+    function formatQuestion(question) {
+        var body = J2M.toJ(question);
+        body = body.replace(/\[[\/]*p\]/g, '');
+        body = body.replace(/\[[\/]*pre\]/g, '');
+        body = body.replace(/\[\/*code\]/g, '{code}');
+        body = body.replace(/\[a[^\]]* href="([^"]*)"[^\]]*\]([^\[]*)\[\/a\]/g, '[$2|$1]');
+        return body;
+    }
+
+    function createIssue(stackexchangeItem) {
+        var body = formatQuestion(stackexchangeItem.body);
+
+        var issue = {
+            "fields": {
+                "project": {
+                    "id": this.issue_template.project_id
+
+                },
+                "summary": stackexchangeItem.title,
+                "description": "{quote}" + body + "{quote} \n\n" + stackexchangeItem.link,
+                "issuetype": {
+                    "name": "Task"
+                },
+                "priority": {
+                    "name": "Minor"
+                }
+            }
+        };
+        return issue;
+    }
+
+}
+
+var init = function (config) {
+    return new IssueUtils(config)
+}
+
+module.exports = init;
